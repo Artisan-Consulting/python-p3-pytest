00d8fe9524e97be16f861b43dbd49e859f4ace64
Artisan <william.kwan@artisanconsultin.nz>
2023-11-19 21:05:04 +1100

    pytest with functions

--- lib/not_none_functions.py
+++ lib/not_none_functions.py
@@ -1,2 +1,2 @@
 def return_not_none():
-    return None
+    return 'return_not_none'
--- lib/string_functions.py
+++ lib/string_functions.py
@@ -1,6 +1,18 @@
+# Error required positional argument: 's'
 def return_string():
-    return ''
-
+    return str()
+    print(type(return_string))
+    
 def interpolate_string(s):
     return f'Hello, {s}!'
     
+def concatenate_string(s1, s2):
+    return s1 + s2
+
+def substring(s, start, end):
+    return s[start:end]
+
+def substring2(s, start):
+    return s[start:]    
+
+
